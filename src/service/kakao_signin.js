1ed0750af7ba6cd44b6eb575bee5a4ec49f727e6
JuHwan <wnghks0531@gmail.com>
2021-04-20 06:52:07 +0900

    service/kakao_signin.js 수정

--- src/service/kakao_signin.js
+++ src/service/kakao_signin.js
@@ -3,15 +3,6 @@ class Kakao {
     window.Kakao.Auth.authorize({
       redirectUri: 'http://localhost:3000/kakao',
     });
-    window.Kakao.API.request({
-      url: '/v2/user/me',
-      success: function (response) {
-        console.log(response);
-      },
-      fail: function (error) {
-        console.log(error);
-      },
-    });
   }
   logout() {
     window.Kakao.Auth.logout();
